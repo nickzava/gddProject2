e0f75d01bda420f72f602f2f19d0670c73dda667
Will Kracke <wjk4218@rit.edu>
2020-09-30 16:41:28 -0400

    Prototype Finished
    Prototype files and code done

---
+++ Assets/Prototype/Manager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField]
+    Tile tileObject;
+
+    public Tile[,] grid = new Tile[2, 3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hardcoded initial setup for the prototype
+        grid[0, 0] = Instantiate(tileObject);
+        grid[0, 0].tileType = 't';
+
+        grid[0, 1] = Instantiate(tileObject);
+        grid[0, 1].tileType = 'x';
+
+        grid[0, 2] = Instantiate(tileObject);
+        grid[0, 2].tileType = 'l';
+
+        grid[1, 0] = Instantiate(tileObject);
+        grid[1, 0].tileType = 'l';
+
+        grid[1, 1] = Instantiate(tileObject);
+        grid[1, 1].tileType = 't';
+
+        grid[1, 2] = Instantiate(tileObject);
+        grid[1, 2].tileType = 'i';
+
+        // Repositions all tiles in the scene
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int n = 0; n < grid.GetLength(1); n++)
+            {
+                grid[i, n].transform.position = new Vector3(-8f + (2.5f * i), 3.5f - (2.5f * n), 0);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grid[0, 0].power = true;
+
+        // Power all adjacent!
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int n = 0; n < grid.GetLength(1); n++)
+            {
+                if (grid[i, n].power)
+                {
+                    PowerAdjacent(i, n);
+                }
+            }
+        }
+    }
+
+    public void PowerAdjacent(int x, int y)
+    {
+        // Runs for each cardinal direction
+        for (int i = 0; i < 5; i++)
+        {
+            // For each direction, first checks if there is a tile in bounds. Then, checks if the sender is facing that direction.
+            switch (i)
+            {
+                case 0:
+                    if (y - 1 >= 0 && y - 1 < grid.GetLength(1))
+                    {
+                        if (grid[x, y].connections[i] == 1 && grid[x, y - 1].connections[2] == 1)
+                        {
+                            grid[x, y - 1].power = true;
+                        }
+                        else
+                        {
+                            grid[x, y - 1].power = false;
+                        }
+                    }
+
+                    break;
+                case 1:
+                    if (x + 1 >= 0 && x + 1 < grid.GetLength(0))
+                    {
+                        if (grid[x, y].connections[i] == 1 && grid[x + 1, y].connections[3] == 1)
+                        {
+                            grid[x + 1, y].power = true;
+                        }
+                        else
+                        {
+                            grid[x + 1, y].power = false;
+                        }
+                    }
+                    break;
+                case 2:
+                    if (y + 1 >= 0 && y + 1 < grid.GetLength(1))
+                    {
+                        if (grid[x, y].connections[i] == 1 && grid[x, y + 1].connections[0] == 1)
+                        {
+                            grid[x, y + 1].power = true;
+                        }
+                        else
+                        {
+                            grid[x, y + 1].power = false;
+                        }
+                    }
+                    break;
+                case 3:
+                    if (x - 1 >= 0 && x - 1 < grid.GetLength(0))
+                    {
+                        if (grid[x, y].connections[i] == 1 && grid[x - 1, y].connections[1] == 1)
+                        {
+                            grid[x - 1, y].power = true;
+                        }
+                        else
+                        {
+                            grid[x - 1, y].power = false;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Prototype/Tile.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField]
+    Sprite XOff;
+    [SerializeField]
+    Sprite XOn;
+    [SerializeField]
+    Sprite IOff;
+    [SerializeField]
+    Sprite IOn;
+    [SerializeField]
+    Sprite TOff;
+    [SerializeField]
+    Sprite TOn;
+    [SerializeField]
+    Sprite LOff;
+    [SerializeField]
+    Sprite LOn;
+
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
+    
+
+    public char tileType;
+    public bool power;
+    // Connections is always a 4-int binary array that starts from the north side and goes clockwise
+    public int[] connections;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Fills out the connection array based on tile type and changes the sprite
+        switch (tileType)
+        {
+            case 'x':
+                connections = new int[] { 1, 1, 1, 1 };
+                spriteRenderer.sprite = XOff;
+                break;
+            case 'i':
+                connections = new int[] { 1, 0, 1, 0 };
+                spriteRenderer.sprite = IOff;
+                break;
+            case 't':
+                connections = new int[] { 0, 1, 1, 1 };
+                spriteRenderer.sprite = TOff;
+                break;
+            case 'l':
+                connections = new int[] { 0, 0, 1, 1 };
+                spriteRenderer.sprite = LOff;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // Checks if powered, then changes sprite accordingly
+        switch(tileType)
+        {
+            case 'x':
+                if (power)
+                    spriteRenderer.sprite = XOn;
+                else
+                    spriteRenderer.sprite = XOff;
+                break;
+            case 'i':
+                if (power)
+                    spriteRenderer.sprite = IOn;
+                else
+                    spriteRenderer.sprite = IOff;
+                break;
+            case 't':
+                if (power)
+                    spriteRenderer.sprite = TOn;
+                else
+                    spriteRenderer.sprite = TOff;
+                break;
+            case 'l':
+                if (power)
+                    spriteRenderer.sprite = LOn;
+                else
+                    spriteRenderer.sprite = LOff;
+                break;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Rotate();
+    }
+
+    // Rotate function changes the orientation and modifies the connection array to reflect the new orientation
+    void Rotate()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, 0, -90f));
+
+        int[] tempArray = new int[4];
+        // Values of temparray become the rotated form of the original array
+        tempArray[0] = connections[3];
+        tempArray[1] = connections[0];
+        tempArray[2] = connections[1];
+        tempArray[3] = connections[2];
+
+        connections = tempArray;
+    }
+
+
+}
