6e181d29abf36fbde1ddb92143155efd694a4c97
Nick Zavattero <64917289+nickzava@users.noreply.github.com>
2020-10-14 18:47:17 -0400

    Dagger traces longest path
    Added dagger tracing longest path + dagger from 4 angles/shadow

--- Assets/Dagger/Script/AthameManager.cs
+++ Assets/Dagger/Script/AthameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AthameManager : MonoBehaviour
@@ -21,17 +22,40 @@ public class AthameManager : MonoBehaviour
     private int direction = 0;
 
     public GameObject dagger;
+    private GameObject daggerObj;
     private Transform daggerTransform;
+
     public GameObject shadow;
+    private GameObject shadowObj;
     private Transform shadowTransform;
 
+    public Sprite leftDagger;
+    public Sprite rightDagger;
+    public Sprite upDagger;
+    public Sprite downDagger;
+
+    private bool daggerIsMoving = false;
+
+    private LinkedList<PathNode> longestPath;
+
+    private float deltaX;
+    private float deltaY;
+    private Vector3 targetPosition = new Vector3(0, 0, 0);
+    private int currentCount = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        daggerTransform = dagger.GetComponent<Transform>().transform;
-        shadowTransform = shadow.GetComponent<Transform>().transform;
+        daggerObj = Instantiate(dagger, new Vector3(0, 0, 0), Quaternion.identity);
+        shadowObj = Instantiate(shadow, new Vector3(0, 0, 0), Quaternion.identity);
+        daggerTransform = daggerObj.GetComponent<Transform>().transform;
+        shadowTransform = shadowObj.GetComponent<Transform>().transform;
+        shadowTransform.Rotate(0, 0, 90);
+        daggerObj.GetComponent<SpriteRenderer>().enabled = false;
+        shadowObj.GetComponent<SpriteRenderer>().enabled = false;
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -54,7 +78,6 @@ public class AthameManager : MonoBehaviour
                 daggerTransform.position = new Vector3(daggerTransform.position.x, daggerTransform.position.y + .0025f, daggerTransform.position.z);
                 shadowTransform.position = new Vector3(shadowTransform.position.x - .0025f, shadowTransform.position.y, shadowTransform.position.z);
             }
-            Debug.Log(Mathf.Abs(daggerTransform.position.y - daggerHeight));
             if(Mathf.Abs(daggerTransform.position.y - daggerHeight) >= 2)
             {
                 raiseDagger = false;
@@ -63,25 +86,80 @@ public class AthameManager : MonoBehaviour
 
         previousIsRaised = isRaised;
 
-        if (Input.GetKey("w"))
-        {
-            daggerTransform.position = new Vector3(daggerTransform.position.x, daggerTransform.position.y + .005f, daggerTransform.position.z);
-            shadowTransform.position = new Vector3(shadowTransform.position.x, shadowTransform.position.y + .005f, shadowTransform.position.z);
-        }
-        if (Input.GetKey("s"))
-        {
-            daggerTransform.position = new Vector3(daggerTransform.position.x, daggerTransform.position.y - .005f, daggerTransform.position.z);
-            shadowTransform.position = new Vector3(shadowTransform.position.x, shadowTransform.position.y - .005f, shadowTransform.position.z);
-        }
-        if (Input.GetKey("a"))
-        {
-            daggerTransform.position = new Vector3(daggerTransform.position.x - .005f, daggerTransform.position.y, daggerTransform.position.z);
-            shadowTransform.position = new Vector3(shadowTransform.position.x - .005f, shadowTransform.position.y, shadowTransform.position.z);
-        }
-        if (Input.GetKey("d"))
-        {
-            daggerTransform.position = new Vector3(daggerTransform.position.x + .005f, daggerTransform.position.y, daggerTransform.position.z);
-            shadowTransform.position = new Vector3(shadowTransform.position.x + .005f, shadowTransform.position.y, shadowTransform.position.z);
+
+        if (daggerIsMoving == true){
+            MoveDagger();
+            //Debug.Log(daggerTransform.position.y  <= targetPosition.y);
+            if (daggerTransform.position.x < targetPosition.x + .005f && daggerTransform.position.x > targetPosition.x - .005f) { 
+                if(daggerTransform.position.y < targetPosition.y + .005f && daggerTransform.position.y > targetPosition.y - .005f)
+                {
+                    if (currentCount < longestPath.Count-1)
+                    {
+                        currentCount++;
+                        SetDaggerDelta(longestPath.ElementAt(currentCount));
+                    }
+                    else
+                    {
+                        daggerIsMoving = false;
+                    }
+                }
+            }
         }
     }
+
+
+    public void StartDaggerMovement()
+    {
+        Debug.Log("Pressed");
+        daggerIsMoving = true;
+        daggerObj.GetComponent<SpriteRenderer>().enabled = true;
+        shadowObj.GetComponent<SpriteRenderer>().enabled = true;
+        daggerObj.GetComponent<Transform>().position = TileManager.Instance.GetTileFromNode(NodeManager.Instance.GetNode(0, 0)).GetComponent<Transform>().position;
+        shadowObj.GetComponent<Transform>().position = TileManager.Instance.GetTileFromNode(NodeManager.Instance.GetNode(0, 0)).GetComponent<Transform>().position;
+        longestPath = NodeManager.Instance.GetNode(0, 0).mPath.GetLongestSequenceInPath();
+        SetDaggerDelta(longestPath.ElementAt(1));
+    }
+
+    void SetDaggerDelta(PathNode target)
+    {
+        targetPosition = TileManager.Instance.GetTileFromNode(target).GetComponent<Transform>().position;
+        deltaY = (targetPosition.y - daggerObj.GetComponent<Transform>().position.y) / 500;
+        deltaX = (targetPosition.x - daggerObj.GetComponent<Transform>().position.x) / 500;
+        float deltaCotan = Convert.ToSingle(Math.Atan2(deltaY, deltaX)/Math.PI);
+        Debug.Log(deltaCotan);
+        if (deltaCotan <= -.25f && deltaCotan > -.75f) //Down
+        {
+            daggerObj.GetComponent<SpriteRenderer>().sprite = downDagger;
+            Debug.Log("down");
+        }
+        if (deltaCotan <= .25f && deltaCotan > -.25f) //Right
+        {
+            daggerObj.GetComponent<SpriteRenderer>().sprite = rightDagger;
+            Debug.Log("right");
+        }
+        if(deltaCotan <= .75f && deltaCotan > .25f) //up
+        {
+            daggerObj.GetComponent<SpriteRenderer>().sprite = upDagger;
+            Debug.Log("up");
+        }
+        if(deltaCotan <= -.75f || deltaCotan > .75f) //Left
+        {
+            daggerObj.GetComponent<SpriteRenderer>().sprite = leftDagger;
+            Debug.Log("left");
+        }
+        
+
+    }
+
+    void MoveDagger()
+    {
+        daggerTransform.position = new Vector3(daggerTransform.position.x + deltaX, daggerTransform.position.y + deltaY, daggerTransform.position.z);
+        shadowTransform.position = new Vector3(shadowTransform.position.x + deltaX, shadowTransform.position.y + deltaY, shadowTransform.position.z);
+    }
+
+    PathNode NextDaggerTarget()
+    {
+        return null;
+    }
 }
+
