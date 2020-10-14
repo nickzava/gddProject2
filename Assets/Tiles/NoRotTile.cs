be1ee8c9a6ec434dc175ce4530f334814dcb202a
Will Kracke <wjk4218@rit.edu>
2020-10-14 16:42:49 -0400

    Added No-Rotate Tile

---
+++ Assets/Tiles/NoRotTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+
+//visual representation of a PathNode
+public class NoRotTile : Tile
+{
+    // No rotate tile -> no functionality on click
+    void OnClick(bool isLeftClick)
+    {
+        Debug.Log("Tried to rotate no-rotate tile");
+    }
+
+    // Still has input in case we want visual feedback for the no rotate tile
+    private void OnMouseOver()
+    {
+        //Left Click
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnClick(true);
+        }
+        //Right Click
+        if (Input.GetMouseButtonDown(1))
+        {
+            OnClick(false);
+        }
+    }
+}
