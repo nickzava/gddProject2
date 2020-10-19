971ab9d32db427ba115a3b3c8f01c9bbd2a3f480
liamgu3 <43424244+liamgu3@users.noreply.github.com>
2020-10-19 12:53:21 -0400

    Next level button and UI scale on screen size

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextLevel : MonoBehaviour
+{
+	private int level = 1;
+	private int maxLevel = 3;
+	GameObject levelSelect; //holds level select UI buttons
+	NodeManager nodeManager;
+	Text tutorialText;      //references Text object in scene
+
+	private void Awake()
+	{
+		levelSelect = GameObject.Find("LevelSelect");
+		nodeManager = GameObject.Find("nodeMan").GetComponent<NodeManager>();   //nodeManager for generating levels
+		tutorialText = GameObject.Find("TutorialTip").GetComponent<Text>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void nextLevelPress()
+	{
+		//setting up next level button
+		string levelString = "Level" + (level + 1);
+		levelSelect.SetActive(true);
+		Debug.Log(levelString);
+		GameStates nextLevelValues = GameObject.Find(levelString).GetComponent<GameStates>();
+		levelSelect.SetActive(false);
+
+		TileManager.Instance.ClearTiles();
+		nodeManager.GenerateLevel(nextLevelValues.seed, nextLevelValues.width, nextLevelValues.height);
+		tutorialText.text = nextLevelValues.tutorialTextString;
+		level = nextLevelValues.level;
+
+		if (level == maxLevel)
+		{
+			//gameObject.SetActive(false);
+			GetComponent<Button>().interactable = false;
+		}
+	}
+}
