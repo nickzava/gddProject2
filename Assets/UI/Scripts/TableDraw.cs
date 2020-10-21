160d74893626036de3b19bc4906cb8f2a5e245dd
Will Kracke <wjk4218@rit.edu>
2020-10-21 17:01:12 -0400

    Added table graphic

---
+++ Assets/UI/Scripts/TableDraw.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableDraw : MonoBehaviour
+{
+    private float tileDimension = 60;
+    private void Awake()
+    {
+        // Dont enable, buttons take care of this
+        //gameObject.SetActive(false);
+    }
+
+    public void DrawTable(int _width, int _height)
+    {
+        // Sets the size
+        float width = (_width * tileDimension) + tileDimension;
+        float height = (_height * tileDimension) + tileDimension;
+
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        gameObject.SetActive(true);
+    }
+
+    public void DisabeTable()
+    {
+        gameObject.SetActive(false);
+    }
+}
