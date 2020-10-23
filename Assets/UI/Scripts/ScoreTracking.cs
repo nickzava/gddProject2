c61375411dbf7a0888f3228adaa8e32922e3736b
Nick Zavattero <64917289+nickzava@users.noreply.github.com>
2020-10-23 16:07:44 -0400

    Update ScoreTracking.cs

--- Assets/UI/Scripts/ScoreTracking.cs
+++ Assets/UI/Scripts/ScoreTracking.cs
@@ -41,6 +41,32 @@ public class ScoreTracking : MonoBehaviour
     /// </summary>
     public void onGUI(bool updateScore)
     {
+        //check(level score / required score) to get easy to understand numbers that work on any level
+        if (requiredScore != 0)
+        {
+            //Debug.Log(levelScore / requiredScore);
+            if (levelScore / requiredScore < 1)
+            {
+                //Insufficient
+                //Debug.Log("insufficient score");
+            }
+            else if (levelScore / requiredScore < 1.3)
+            {
+                //bare minimum pass
+                //Debug.Log("bare minimum score");
+            }
+            else if (levelScore / requiredScore < 1.6)
+            {
+                //better
+                //Debug.Log("Above and beyond score");
+            }
+            else
+            {
+                //Best+
+                //Debug.Log("Extremely high score");
+            }
+        }
+
         totalScoreLabel.text = "Total Score:";
         levelScoreLabel.text = "Level Score:";
 		if (updateScore)
